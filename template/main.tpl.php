70aed4bbd84b0233f9a9fe61b07a960632b82a81
taylorchu <tailinchu@gmail.com>
2010-10-02 12:22:24 -0700

    move goolog to github

--- template/main.tpl.php
+++ template/main.tpl.php
@@ -10,6 +10,6 @@
 <h2><?php echo $data['head'];?></h2>
 <h5><a href="index.php?post"><?php echo $lang['post'];?></a> | <a href="index.php?comment"><?php echo $lang['comment'];?></a> | <a href="index.php?more"><?php echo $lang['more'];?></a> | <?php echo isset($_SESSION['admin'])? '<a href="config.php">'.$lang['config'].'</a> | <a href="auth.php?logout">'.$lang['logout'].'</a>' : '<a href="auth.php?login">'.$lang['login'].'</a>';?></h5>
 <?php echo $data['body'];?>
-<h4><?php echo $lang['powered_by']?> <a href="http://code.google.com/p/goolog/">Goolog</a> | <a href="feed.php"><?php echo $lang['feed']?></a></h4>
+<h4><?php echo $lang['powered_by']?> <a href="http://github.com/goolog/">Goolog</a> | <a href="feed.php"><?php echo $lang['feed']?></a></h4>
 </body>
 </html>
